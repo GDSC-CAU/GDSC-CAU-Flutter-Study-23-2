2d915b2237f4c451de0c4f2de57398dca6a5c204
hyun-hyang <jimin3060@naver.com>
2023-10-30 02:08:05 +0900

    1.2 Dynamic Type

--- hyun-hyang/main.dart
+++ hyun-hyang/main.dart
@@ -10,4 +10,19 @@ void main() {
   // class에서 변수나 속성 선언시
   String name2 = '지민';
   name = 'jimin';
+
+  // dynamic은 필요한 경우만 사용
+  // 조건문으로 타입 확인시, 타입의 메소드 사용 가능
+
+  var name3;
+  name3 = 'nico';
+  name3 = 12;
+  name3 = true;
+
+  dynamic name4;
+  if (name4 is String) {}
+  if (name4 is int) {}
+  name4 = 'nico';
+  name4 = 12;
+  name4 = true;
 }
