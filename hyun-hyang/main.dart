859b84d565dd6ebb30b70d79cca734eaf9a9c3ea
hyun-hyang <jimin3060@naver.com>
2023-10-30 02:03:33 +0900

    #1.1 The Var Keyword

--- hyun-hyang/main.dart
+++ hyun-hyang/main.dart
@@ -1,12 +1,13 @@
 void main() {
-  //변수 선언 2가지 방법
+  // #1.1 The Var Keyword
+  // 변수 선언 2가지 방법
 
-  //업데이트 가능
-  //함수나 메소드 내부에 지역변수 선언시
+  // 업데이트 가능
+  // 함수나 메소드 내부에 지역변수 선언시
   var name = '지민';
   name = 'jimin';
 
-  //class에서 변수나 속성 선언시
+  // class에서 변수나 속성 선언시
   String name2 = '지민';
   name = 'jimin';
 }
