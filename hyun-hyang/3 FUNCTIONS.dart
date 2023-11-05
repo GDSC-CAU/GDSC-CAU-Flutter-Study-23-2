48bcebd1f92c674581df6a106911f1d50bcc020f
hyun-hyang <jimin3060@naver.com>
2023-11-06 01:20:15 +0900

    3.1 Named Parameters

--- hyun-hyang/3 FUNCTIONS.dart
+++ hyun-hyang/3 FUNCTIONS.dart
@@ -1,15 +1,26 @@
+// #3.0 Defining a Function (04:15)
 String sayHello(String potato) => "Hello $potato nice to meet you!"; //only one line //바로 return
 
-num plus(num a, num b) => a + b;
+num plus(num a, num b) => a + b; //num은 int와 double 둘다 작동함
+
+// #3.1 Named Parameters (08:54)
+//non nullvalue //null safety //set default value
+String sayHello2({String name = 'anon', int age = 99, String country = 'wakanda',}){ 
+}
+//required modifier
+String sayHello3({required String name, required int age, required String country,}){
+  return "Hello $name, you are $age, and you come from $country";
+}
 
 void main(){
-
     // #3 FUNCTIONS
-    
-    // #3.0 Defining a Function (04:15)
     print(sayHello('nico'));
 
-    // #3.1 Named Parameters (08:54)
+    print(sayHello2( //순서 상관없이 부르기
+      age: 12,
+      country: 'cuba',
+      name: 'nico'
+    ));
 
     // #3.2 Recap (05:49)
 
